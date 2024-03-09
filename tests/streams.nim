71562b92764548ff34080e1952530b482cccbd12
Victor Brestoiu <vabresto@uwaterloo.ca>
2024-03-09 13:35:47 -0500

    Clean up example a bit

--- tests/streams.nim
+++ tests/streams.nim
@@ -3,32 +3,46 @@ import std/os
 
 import nim_redis
 
-import nim_redis/core/redis_value
-import nim_redis/core/client as redis_client
-import nim_redis/core/serialization/parser
-import nim_redis/core/serialization/serializer
-import nim_redis/core/streams/socketstreams
 
 let client = newRedisClient("localhost", pass=some "foobarabc123")
 let clientPtr = client.addr
 
 
+type
+  StreamResponse = object
+    stream: string
+    id: string
+    contents: RedisValue
+
+
+proc parseStreamResponse(val: RedisValue): ?!StreamResponse {.raises: [].} =
+  var resp = StreamResponse()
+  try:
+    resp.stream = val.arr[0].arr[0].str
+    resp.id = val.arr[0].arr[1].arr[0].arr[0].str
+    resp.contents = val.arr[0].arr[1].arr[0].arr[1]
+    return success resp
+  except ValueError:
+    return failure "Error parsing as a stream response: " & $val
+
+
 proc receiveThreadProc() {.thread, gcsafe.} =
+  var lastId = "$"
+
   while true:
-    clientPtr[].send(@["XREAD", "BLOCK", "0", "STREAMS", "race:france", "$"])
+    clientPtr[].send(@["XREAD", "BLOCK", "0", "STREAMS", "race:france", lastId])
     let replyRaw = clientPtr[].receive()
-    # echo "Got raw reply"
     if replyRaw.isOk:
-      echo "FINAL: ", replyRaw[]
-    # else:
-    #   echo replyRaw.error.msg
-      
-    
+      let replyParseAttempt = replyRaw[].parseStreamResponse
+      if replyParseAttempt.isOk:
+        let reply = replyParseAttempt[]
+
+        lastId = reply.id
+        echo "PARSED: ", reply
+
 
 var receiveThread: Thread[void]
 createThread(receiveThread, receiveThreadProc)
-
-# discard client.cmd(@["XREAD", "BLOCK", "0", "STREAMS", "race:france", "$"])
 
 sleep(50_000)
 client.close()
